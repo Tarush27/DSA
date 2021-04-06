c3e14a27f4d4ffe8ea304e9548e591b1bbe47022
Tarush Kaistha <tarushkaistha05@gmail.com>
2021-04-07 00:13:37 +0530

    queue basics

---
+++ src/Collection/Queue/MainClass.java
@@ -0,0 +1,20 @@
+package Collection.Queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MainClass {
+    public static void main(String[] args){
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.offer(4);
+        q.offer(5);
+        q.offer(6);
+        q.remove();
+        q.poll();
+        q.element();
+        q.peek();
+    }
+}
