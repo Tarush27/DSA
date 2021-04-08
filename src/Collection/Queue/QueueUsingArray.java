e220233ffa7907aa52e60a2a0f7ce8872db09f61
Tarush27 <tarushkaistha05@gmail.com>
2021-04-09 01:02:11 +0530

    implemented queue using array with basic functions enqueue and dequeue

---
+++ src/Collection/Queue/QueueUsingArray.java
@@ -0,0 +1,53 @@
+package Collection.Queue;
+
+public class QueueUsingArray {
+    private static int front, rear, capacity;
+    private static int[] queue;
+
+    QueueUsingArray(int c) {
+        front = rear = 0;
+        capacity = c;
+        queue = new int[capacity];
+    }
+
+    static void enqueue(int item) {
+        if (rear == capacity) {
+            System.out.println("Overflow");
+            return;
+        } else {
+            queue[rear] = item;
+            rear++;
+        }
+        return;
+    }
+
+    static int dequeue() {
+        int y = 0;
+        if (front == rear) {
+            System.out.println("Underflow");
+        } else {
+            y = queue[front];
+            front++;
+        }
+        return y;
+    }
+
+    static void show() {
+        if (front == rear) {
+            System.out.println("Queue empty");
+            return;
+        }
+        for (int i = front; i < rear; i++) {
+            System.out.println(queue[i]);
+        }
+    }
+
+    static void front() {
+        if (front == rear) {
+            System.out.println("Queue empty");
+            return;
+        }
+        System.out.println("Front is : " + queue[front]);
+        return;
+    }
+}
