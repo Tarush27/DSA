8d394d236658e5f108b31fbb2070099fc68b38dd
Tarush27 <tarushkaistha05@gmail.com>
2021-04-08 09:16:16 +0530

    queue

--- src/Collection/Queue/MainClass.java
+++ src/Collection/Queue/MainClass.java
@@ -5,21 +5,21 @@ import java.util.Queue;
 
 public class MainClass {
     public static void main(String[] args){
-        Queue<Integer> q = new LinkedList<>();
-        q.add(1);
-        q.add(2);
-        q.add(3);
-        q.offer(4);
-        q.offer(5);
-        q.offer(6);
-        q.remove();
-        q.poll();
-        q.element();
-        q.peek();
+//        Queue<Integer> q = new LinkedList<>();
+//        q.add(1);
+//        q.add(2);
+//        q.add(3);
+//        q.offer(4);
+//        q.offer(5);
+//        q.offer(6);
+//        q.remove();
+//        q.poll();
+//        q.element();
+//        q.peek();
         MyQueue<Integer> mq = new MyQueue<>();
         mq.enqueue(10);
-        mq.enqueue(11);
-        mq.enqueue(12);
-        mq.dequeue();
+//        mq.enqueue(11);
+//        mq.enqueue(12);
+//        mq.dequeue();
     }
 }
