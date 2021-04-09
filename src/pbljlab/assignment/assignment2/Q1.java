544fc52a4bb7d0cdebc46d813638b92d863fafb0
Tarush27 <tarushkaistha05@gmail.com>
2021-04-09 10:12:42 +0530

    code of jdbc connection using sql

---
+++ src/pbljlab/assignment/assignment2/Q1.java
@@ -0,0 +1,22 @@
+package pbljlab.assignment.assignment2;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Q1 {
+    public static void main(String[] args) throws SQLException {
+        Connection conn = null;
+
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "scott", "tiger");
+            System.out.println("Connection Established successfully");
+        } catch (Exception e) {
+            System.out.println("Connection could  not be established");
+        } finally {
+            if (conn != null) conn.close();
+        }
+    }
+
+}
