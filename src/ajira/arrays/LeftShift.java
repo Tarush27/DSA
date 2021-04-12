b223e1140e38f7a2dbe82384ab4c2f46a6dda254
Tarush Kaistha <tarushkaistha05@gmail.com>
2021-04-12 08:24:42 +0530

    modification of for loop

--- src/ajira/arrays/LeftShift.java
+++ src/ajira/arrays/LeftShift.java
@@ -1,19 +1,23 @@
 package ajira.arrays;
 
 public class LeftShift {
-    public static void main(String ...args){
-        int[] a = {1,2,3,4,5};
+    public static void main(String ...args) {
+        int[] a = {1, 2, 3, 4, 5};
         int n = 1;
-        for (int i = 0; i < n; i++) {
-            int temp = a[0];
-            for (int j = 0;j< a.length-1;j++) {
-                a[j] = a[j + 1];
-            }
-            a[i] = temp;
+        for (int i = 0;i < a.length;i++){
+            System.out.println(a[i] + " ");
         }
-        System.out.println();
+        for (int i = 0;i < n;i++){
+            int j,temp;
+            temp = a[0];
+            for (j = 0;j< a.length-1;j++){
+                a[j] = a[j+1];
+            }
+            a[j] = temp;
+        }
+        System.out.println("array is : ");
         for (int i = 0;i< a.length;i++){
-            System.out.println(a[i]);
+            System.out.println(a[i] + " ");
         }
     }
 }
