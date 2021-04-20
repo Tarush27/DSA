5f677b5622de624f58f0e93ffe97726642787df3
Tarush27 <tarushkaistha05@gmail.com>
2021-04-20 13:42:05 +0530

    thread using thread class in java

---
+++ src/threads/ThreadDemo.java
@@ -0,0 +1,12 @@
+package threads;
+
+public class ThreadDemo extends Thread{  // extending with the thread class
+    public void run(){   // run function
+        System.out.println("thread runs");
+    }
+    public static void main(String ...args){ // main function
+        ThreadDemo td = new ThreadDemo();  // creating object of the thread class
+        td.start();   // calling start method to start thread coming into runnable state
+        System.out.println("welcome to thread");
+    }
+}
