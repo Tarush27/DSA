6eadba6b4f7de27e874fae1035f46d7b2bd59d96
Tarush27 <tarushkaistha05@gmail.com>
2021-04-24 13:38:37 +0530

    added run() to explain thread life cycle

--- src/threads/ThreadDemo.java
+++ src/threads/ThreadDemo.java
@@ -7,6 +7,7 @@ public class ThreadDemo extends Thread{  // extending with the thread class
     public static void main(String ...args){ // main function
         ThreadDemo td = new ThreadDemo();  // creating object of the thread class
         td.start();   // calling start method to start thread coming into runnable state
+        td.run();
         System.out.println("welcome to thread");
     }
 }
